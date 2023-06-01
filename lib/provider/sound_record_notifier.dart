8f50d9e97c33b164196606208375a3dc71d6f71f
subhi khalifeh <subhikhalifeh1999@gmail.com>
2023-06-01 16:12:08 +0300

    fix create file error

--- lib/provider/sound_record_notifier.dart
+++ lib/provider/sound_record_notifier.dart
@@ -120,7 +120,6 @@ class SoundRecordNotifier extends ChangeNotifier {
       _sdPath = initialStorePathRecord.isEmpty
           ? tempDir.path
           : initialStorePathRecord;
-
     var d = Directory(_sdPath);
     if (!d.existsSync()) {
       d.createSync(recursive: true);
